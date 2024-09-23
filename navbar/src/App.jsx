318f349f4bf57c32dc86ca32d78942d885a69dce
Daniela Oberti <ddanioberti@gmail.com>
2024-09-23 12:50:28 -0300

    first commit

---
+++ navbar/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <div><Navbar />
+        <main>
+            <h1>Welcome</h1>
+        </main>
+    </div>
+
+  )
+}
+
+export default App
